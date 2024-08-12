6b02d25f817385204e960922ab76d735663bb77f
YogeshPrajapatii <verma981133@gmail.com>
2024-08-12 21:38:09 +0530

    aFirst Commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int s = 5;
+        int f = 152894;
+       
+        int z = s+f;
+        System.out.println(z);
+    }
+}
